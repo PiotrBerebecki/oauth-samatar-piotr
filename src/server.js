ae46584d3552d7fa13c9aa766f665f33651fcee0
Samatar Axmed <Samatar_267@hotmail.com>
2017-04-10 11:49:41 +0100

    Created server and installed dependencies Relates to issue #1

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const hapi = require('hapi');
+const inert = require('inert');
+const server = new hapi.Server();
+const routes = require('./routes');
+
+
+server.connection({
+  port: process.env.PORT || 3000
+});
+
+server.register([inert], (err)=>{
+  if (err) throw err;
+
+  server.route(routes);
+
+});
+
+module.exports = server;
