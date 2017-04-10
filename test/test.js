9da0c8c44b2e3b0d6d4d7976ffb9b6e17f794196
PiotrBerebecki <petebere@gmail.com>
2017-04-10 12:52:57 +0100

    Setup login redirect
    fixes #8

---
+++ test/test.js
@@ -0,0 +1,22 @@
+const server = require('./../src/server.js');
+const tape = require('tape');
+const querystring = require('querystring');
+
+
+tape('login redirect', (t) => {
+  const options = {
+    method: 'GET',
+    url: '/login'
+  };
+
+  server.inject(options, res => {
+    t.equal(res.statusCode, 302, 'statusCode should be 302');
+
+    const redirectUrlParams = querystring.parse(res.headers.location);
+    const { redirect_uri }  = redirectUrlParams;
+    const expectedRedirectUri = 'https://localhost:3000/welcome';
+
+    t.equal(redirect_uri, expectedRedirectUri, `the redirect_uri should be ${expectedRedirectUri}`);
+    t.end();
+  });
+});
